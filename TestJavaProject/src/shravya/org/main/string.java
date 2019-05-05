05cb9a66831b1572745a654dc452ea337a4c346d
shravyanayak <shravya29071997@gmail.com>
2019-05-05 17:56:30 +0530

    strings

---
+++ TestJavaProject/src/shravya/org/main/string.java
@@ -0,0 +1,10 @@
+package shravya.org.main;
+
+public class string {
+	public static void main(String[] args){
+		String s1="book";
+		String s2="name";
+		String s3=s1+s2;
+		System.out.println(s3);
+	}
+}
